92209719362fbb083112b974fcd40da4b365e5e8
duggavo <>
2023-06-28 14:23:33 +0200

    Flight improvements

--- src/main/java/net/potatohack/hacks/FlightHack.java
+++ src/main/java/net/potatohack/hacks/FlightHack.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2023 duggavo.
  * Copyright (c) 2014-2023 Wurst-Imperium and contributors.
  *
  * This source code is subject to the terms of the GNU General Public
@@ -11,7 +12,6 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import net.potatohack.Category;
 import net.potatohack.SearchTags;
-import net.potatohack.events.AirStrafingSpeedListener;
 import net.potatohack.events.IsPlayerInWaterListener;
 import net.potatohack.events.UpdateListener;
 import net.potatohack.hack.Hack;
@@ -21,15 +21,15 @@ import net.potatohack.settings.SliderSetting.ValueDisplay;
 
 @SearchTags({"FlyHack", "fly hack", "flying"})
 public final class FlightHack extends Hack
-	implements UpdateListener, IsPlayerInWaterListener, AirStrafingSpeedListener
+	implements UpdateListener, IsPlayerInWaterListener
 {
 	public final SliderSetting horizontalSpeed = new SliderSetting(
-		"Horizontal Speed", 1, 0.05, 10, 0.05, ValueDisplay.DECIMAL);
+		"Horizontal Speed", 2, 0.05, 10, 0.05, ValueDisplay.DECIMAL);
 	
 	public final SliderSetting verticalSpeed = new SliderSetting(
 		"Vertical Speed",
 		"\u00a7c\u00a7lWARNING:\u00a7r Setting this too high can cause fall damage, even with NoFall.",
-		1, 0.05, 5, 0.05, ValueDisplay.DECIMAL);
+		1.4, 0.05, 5, 0.05, ValueDisplay.DECIMAL);
 	
 	private final CheckboxSetting slowSneaking = new CheckboxSetting(
 		"Slow sneaking",
@@ -38,13 +38,13 @@ public final class FlightHack extends Hack
 	
 	private final CheckboxSetting antiKick = new CheckboxSetting("Anti-Kick",
 		"Makes you fall a little bit every now and then to prevent you from getting kicked.",
-		false);
+		true);
 	
 	private final SliderSetting antiKickInterval =
 		new SliderSetting("Anti-Kick Interval",
 			"How often Anti-Kick should prevent you from getting kicked.\n"
 				+ "Most servers will kick you after 80 ticks.",
-			30, 5, 80, 1, ValueDisplay.INTEGER.withSuffix(" ticks"));
+			50, 5, 80, 1, ValueDisplay.INTEGER.withSuffix(" ticks"));
 	
 	private final SliderSetting antiKickDistance = new SliderSetting(
 		"Anti-Kick Distance",
@@ -53,6 +53,7 @@ public final class FlightHack extends Hack
 		0.07, 0.01, 0.2, 0.001, ValueDisplay.DECIMAL.withSuffix("m"));
 	
 	private int tickCounter = 0;
+	private double antiKickPreviousSpeed = 0;
 	
 	public FlightHack()
 	{
@@ -70,13 +71,13 @@ public final class FlightHack extends Hack
 	public void onEnable()
 	{
 		tickCounter = 0;
+		antiKickPreviousSpeed = 0;
 		
 		WURST.getHax().creativeFlightHack.setEnabled(false);
 		WURST.getHax().jetpackHack.setEnabled(false);
 		
 		EVENTS.add(UpdateListener.class, this);
 		EVENTS.add(IsPlayerInWaterListener.class, this);
-		EVENTS.add(AirStrafingSpeedListener.class, this);
 	}
 	
 	@Override
@@ -84,45 +85,90 @@ public final class FlightHack extends Hack
 	{
 		EVENTS.remove(UpdateListener.class, this);
 		EVENTS.remove(IsPlayerInWaterListener.class, this);
-		EVENTS.remove(AirStrafingSpeedListener.class, this);
 	}
 	
 	@Override
 	public void onUpdate()
 	{
 		ClientPlayerEntity player = MC.player;
-		
+		Vec3d vel = player.getVelocity();
 		player.getAbilities().flying = false;
 		
-		player.setVelocity(0, 0, 0);
-		Vec3d velocity = player.getVelocity();
-		
-		if(MC.options.jumpKey.isPressed())
-			player.setVelocity(velocity.x, verticalSpeed.getValue(),
-				velocity.z);
-		
-		if(MC.options.sneakKey.isPressed())
-			player.setVelocity(velocity.x, -verticalSpeed.getValue(),
-				velocity.z);
-		
+		double velx = vel.x, velz = vel.z;
+		double speed = horizontalSpeed.getValue() / 10;
+
+		double yaw = -Math.toRadians(player.getYaw()) - (Math.PI/2);
+
+		boolean didMove = false;
+
+		if (MC.options.forwardKey.isPressed()) {
+			didMove = true;
+			velx += ((float)Math.cos(yaw) * speed);
+			velz -= ((float)Math.sin(yaw) * speed);
+		}
+		if (MC.options.backKey.isPressed()) {
+			didMove = true;
+			velx -= ((float)Math.cos(yaw) * speed);
+			velz += ((float)Math.sin(yaw) * speed);
+		}
+		if (MC.options.leftKey.isPressed()) {
+			didMove = true;
+			velx += ((float)Math.cos(yaw+(Math.PI/2)) * speed);
+			velz -= ((float)Math.sin(yaw+(Math.PI/2)) * speed);
+		}
+		if (MC.options.rightKey.isPressed()) {
+			didMove = true;
+			velx += ((float)Math.cos(yaw-(Math.PI/2)) * speed);
+			velz -= ((float)Math.sin(yaw-(Math.PI/2)) * speed);
+		}
+
+		if (didMove) {
+			// Make sure that the speed never exceeds limit
+			double currentSpeed = Math.sqrt(velx*velx + velz*velz);
+
+			double maxSpeed = horizontalSpeed.getValue();
+			if (MC.options.sneakKey.isPressed() && slowSneaking.isChecked())
+				maxSpeed = 0.85;
+
+			double multiplier = maxSpeed / currentSpeed;
+			if (multiplier < 1) {
+				velx *= multiplier;
+				velz *= multiplier;
+			}
+		} else {
+			velx *= 0.6;
+			velz *= 0.6;
+		}
+
+
+		if (MC.options.jumpKey.isPressed())
+			player.setVelocity(vel.x, getVertSpeed(vel.y, verticalSpeed.getValue()), vel.z);
+		else if (MC.options.sneakKey.isPressed())
+			player.setVelocity(vel.x, getVertSpeed(vel.y, -verticalSpeed.getValue()), vel.z);
+		else
+			player.setVelocity(velx, 0, velz);
+
 		if(antiKick.isChecked())
-			doAntiKick(velocity);
+			doAntiKick(vel);
 	}
 	
-	@Override
-	public void onGetAirStrafingSpeed(AirStrafingSpeedEvent event)
-	{
-		float speed = horizontalSpeed.getValueF();
+	private double getVertSpeed(double speed, double toAdd) {
+		if (MC.player.isTouchingWater() && !WURST.getHax().fishHack.isEnabled()) {
+			speed += 0.005; // Remove underwater gravity
+		} else if (!MC.player.isTouchingWater()) {
+			speed += 0.08; // Remove gravity
+		}
+		speed += toAdd / 10;
 		
-		if(MC.options.sneakKey.isPressed() && slowSneaking.isChecked())
-			speed = Math.min(speed, 0.85F);
-		
-		event.setSpeed(speed);
+		speed = Math.max(speed, -toAdd);
+		speed = Math.min(speed, toAdd);
+
+		return speed;
 	}
-	
-	private void doAntiKick(Vec3d velocity)
+
+	private void doAntiKick(Vec3d vel)
 	{
-		if(tickCounter > antiKickInterval.getValueI() + 1)
+		if(tickCounter > antiKickInterval.getValueI() + 2)
 			tickCounter = 0;
 		
 		switch(tickCounter)
@@ -130,14 +176,19 @@ public final class FlightHack extends Hack
 			case 0 ->
 			{
 				if(MC.options.sneakKey.isPressed())
-					tickCounter = 2;
-				else
-					MC.player.setVelocity(velocity.x,
-						-antiKickDistance.getValue(), velocity.z);
+					tickCounter = 3;
+				else {
+					antiKickPreviousSpeed = vel.y;
+					if (antiKickPreviousSpeed <= antiKickDistance.getValue()) {
+						antiKickPreviousSpeed = 0;
+					} 
+					MC.player.setVelocity(vel.x,
+						-antiKickDistance.getValue(), vel.z);
+				}
 			}
-			
-			case 1 -> MC.player.setVelocity(velocity.x,
-				antiKickDistance.getValue(), velocity.z);
+			case 1 -> MC.player.setVelocity(vel.x,
+				antiKickDistance.getValue(), vel.z);
+			case 2 -> MC.player.setVelocity(vel.x, antiKickPreviousSpeed, vel.z);
 		}
 		
 		tickCounter++;
